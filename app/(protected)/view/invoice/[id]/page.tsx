0a34206e8a0f3fd49b4e71146e20df41c507e22e
Bogdan <moale.bogdan.ionica@gmail.com>
2024-12-24 15:45:32 +0200

    Refactor invoice page templates to use secure cookie

--- app/(protected)/view/invoice/[id]/page.tsx
+++ app/(protected)/view/invoice/[id]/page.tsx
@@ -10,16 +10,21 @@ async function fetchInvoice(
   invoiceId: string
 ): Promise<InvoiceTemplateModelProps> {
   const getCookie = async (name: string) => {
+    const secureCookie = cookies().get(`__Secure-${name}`);
+    if (secureCookie?.value) return secureCookie.value;
+
     return cookies().get(name)?.value ?? "";
   };
-
   const sessionTokenAuthJs = await getCookie("authjs.session-token");
   const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/invoices/get/${invoiceId}`;
+  const isProduction = process.env.NODE_ENV === "production";
 
   const res = await fetch(apiUrl, {
     method: "GET",
     headers: {
-      Cookie: `authjs.session-token=${sessionTokenAuthJs}`,
+      Cookie: isProduction
+        ? `__Secure-authjs.session-token=${sessionTokenAuthJs}`
+        : `authjs.session-token=${sessionTokenAuthJs}`,
     },
   });
 
