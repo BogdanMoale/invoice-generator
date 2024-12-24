507628dece61b8bd7fcf7e3759ef15c651841dcc
Bogdan <moale.bogdan.ionica@gmail.com>
2024-12-24 15:30:20 +0200

    Refactor InvoicesPage component to conditionally set cookie header based on environment and remove unnecessary logging

--- app/(protected)/invoices/page.tsx
+++ app/(protected)/invoices/page.tsx
@@ -20,20 +20,14 @@ const InvoicesPage = async ({
 }: {
   searchParams: { page?: string };
 }) => {
-  console.log("Starting InvoicesPage component");
-
   const getCookie = async (name: string) => {
-    // Try production cookie name first (with _Secure- prefix)
     const secureCookie = cookies().get(`__Secure-${name}`);
     if (secureCookie?.value) return secureCookie.value;
 
-    // Fallback to development cookie name
     return cookies().get(name)?.value ?? "";
   };
 
   const sessionTokenAuthJs = await getCookie("authjs.session-token");
-
-  console.log("Session token retrieved:", sessionTokenAuthJs);
 
   const currentPage = searchParams.page ? parseInt(searchParams.page) : 1;
   const skip = (currentPage - 1) * 10;
@@ -42,19 +36,20 @@ const InvoicesPage = async ({
   let initialInvoices: InvoiceWithCustomer[] = [];
   let totalCount: number = 0;
 
+  const isProduction = process.env.NODE_ENV === "production";
+
   try {
     const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/invoices/get/?skip=${skip}&take=${take}`;
-    console.log("Fetching from URL:", apiUrl);
 
     const res = await fetch(apiUrl, {
       method: "GET",
       headers: {
-        Cookie: `__Secure-authjs.session-token=${sessionTokenAuthJs}`,
+        Cookie: isProduction
+          ? `__Secure-authjs.session-token=${sessionTokenAuthJs}`
+          : `authjs.session-token=${sessionTokenAuthJs}`,
       },
       cache: "no-store", // Ensures data is fresh on each fetch
     });
-
-    console.log("Fetch response status:", res.status);
 
     if (!res.ok) {
       const errorText = await res.text();
@@ -65,15 +60,13 @@ const InvoicesPage = async ({
     }
 
     const data = await res.json();
-    console.log("Data received:", JSON.stringify(data).slice(0, 100) + "...");
+
     initialInvoices = data.invoices;
     totalCount = data.totalCount;
   } catch (error) {
     console.error("Error in try-catch block:", error);
     throw new Error("Error fetching invoices");
   }
-
-  console.log("Rendering InvoicesPage component");
 
   return (
     <Suspense
