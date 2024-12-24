ec4bdb917eac89a7894b3fceb7aac871a2dc5c23
Bogdan <moale.bogdan.ionica@gmail.com>
2024-12-24 14:56:01 +0200

    Add logging and refactor API URL construction in InvoicesPage component

--- app/(protected)/invoices/page.tsx
+++ app/(protected)/invoices/page.tsx
@@ -3,7 +3,6 @@ import { Invoice, Customer } from "@prisma/client";
 import { Spinner } from "@nextui-org/spinner";
 import Invoices from "@/components/invoices/invoices";
 import { cookies } from "next/headers";
-import { headers } from "next/headers";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -21,6 +20,8 @@ const InvoicesPage = async ({
 }: {
   searchParams: { page?: string };
 }) => {
+  console.log("Starting InvoicesPage component");
+
   const getCookie = async (name: string) => {
     // Try production cookie name first (with _Secure- prefix)
     const secureCookie = cookies().get(`_Secure-${name}`);
@@ -31,6 +32,10 @@ const InvoicesPage = async ({
   };
 
   const sessionTokenAuthJs = await getCookie("authjs.session-token");
+  console.log(
+    "Session token retrieved:",
+    sessionTokenAuthJs ? "Present" : "Not present"
+  );
 
   const currentPage = searchParams.page ? parseInt(searchParams.page) : 1;
   const skip = (currentPage - 1) * 10;
@@ -40,21 +45,18 @@ const InvoicesPage = async ({
   let totalCount: number = 0;
 
   try {
-    // Get the host from headers
-    const headersList = headers();
-    const host = headersList.get("host") || "";
-    const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
-
-    // Construct the API URL using the current host
-    const apiUrl = `${protocol}://${host}/api/invoices/get/?skip=${skip}&take=${take}`;
+    const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/invoices/get/?skip=${skip}&take=${take}`;
+    console.log("Fetching from URL:", apiUrl);
 
     const res = await fetch(apiUrl, {
       method: "GET",
       headers: {
-        Cookie: `_Secure-authjs.session-token=${sessionTokenAuthJs}`,
+        Cookie: `authjs.session-token=${sessionTokenAuthJs}`,
       },
-      cache: "no-store",
+      cache: "no-store", // Ensures data is fresh on each fetch
     });
+
+    console.log("Fetch response status:", res.status);
 
     if (!res.ok) {
       const errorText = await res.text();
@@ -65,12 +67,15 @@ const InvoicesPage = async ({
     }
 
     const data = await res.json();
+    console.log("Data received:", JSON.stringify(data).slice(0, 100) + "...");
     initialInvoices = data.invoices;
     totalCount = data.totalCount;
   } catch (error) {
     console.error("Error in try-catch block:", error);
     throw new Error("Error fetching invoices");
   }
+
+  console.log("Rendering InvoicesPage component");
 
   return (
     <Suspense
